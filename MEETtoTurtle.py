43d28e096a34ef8ae169a9a5d8c34f1ab9234517
dan19-meet <dan19@meet.mit.edu>
2017-07-23 18:47:42 +0300

    Stopping here

---
+++ MEETtoTurtle.py
@@ -0,0 +1,68 @@
+import turtle
+
+turtle.penup()
+
+turtle.goto(-200,-100)
+turtle.pendown()
+
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+
+turtle.penup()
+
+turtle.goto(-80, 100)
+turtle.pendown()
+
+turtle.goto(-80, -100)
+turtle.goto(20, -100)
+
+turtle.penup()
+turtle.goto(-80, 0);
+turtle.pendown()
+
+turtle.goto(20, 0)
+
+turtle.penup()
+turtle.goto(-80, 100)
+
+turtle.pendown()
+
+turtle.goto(20, 100)
+
+turtle.penup()
+turtle.goto(40, 100)
+turtle.pendown()
+
+turtle.goto(140, 100)
+
+turtle.penup()
+turtle.goto(40, 100)
+turtle.pendown()
+
+turtle.goto(40, -100)
+turtle.goto(140, -100)
+
+turtle.penup()
+turtle.goto(40, 0)
+turtle.pendown()
+
+turtle.goto(140, 0)
+
+turtle.penup()
+turtle.goto(160, 100)
+turtle.pendown()
+
+turtle.goto(260, 100)
+
+turtle.penup()
+turtle.goto(210, 100)
+turtle.pendown()
+
+turtle.goto(210, -100)
+
+
+
+
+turtle.mainloop()
--- README.md
+++
@@ -1,2 +0,0 @@
-# meet2017y1lab1
-Day 1 lab, MEET Y1, Class of 2019
